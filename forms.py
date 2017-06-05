57714eea1aba23510ab3b3251ced1204a04c924e
oamo01 <omar.mariscal@nimanic.com>
2017-06-04 20:10:20 -0500

    Update 3

--- forms.py
+++ forms.py
@@ -6,6 +6,6 @@ from wtforms import validators
 
 class CommentForm(Form):
 	fl = FloatField('Flujo', [validators.NumberRange(min=0, max=5.02, message='Introduce un valor en el intervalo de 0 a 5.02.')
-									, validators.Required(message='Introduce un numero')])
+									, validators.Required(message='Introduce un numero entre 0 y 5.02')])
 	
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 
 title = "LABINTAHP"
 flag=0
+f=0
 #port='/dev/tty.wchusbserial14220'
 #nodo=2
 #variador = minimalmodbus.Instrument(port, nodo)
@@ -16,6 +17,7 @@ def Activar():
 
 def Desactivar():
 	acciones.Desactivar()
+	global flag
 	flag=0
 
 app = Flask(__name__)
@@ -25,6 +27,7 @@ def index():
 	comment_form = forms.CommentForm(request.form)
 	if request.method == 'POST' and comment_form.validate():
 		if flag == 1:
+			global f
 			print flag
 			a = comment_form.fl.data
 			f = acciones.Conversion(a)
@@ -40,11 +43,11 @@ def activate():
 	
 	if request.method == 'POST' and flag == 0:
 		Activar()
-		return "Variador de frecuencia se ha activado"
+		return "El variador de frecuencia se ha activado"
 
 	elif request.method == 'POST' and flag == 1:
 		Desactivar()
-		return "Variador de frecuencia se ha desactivado"
+		return "El variador de frecuencia se ha desactivado"
 	return render_template('index.html', title = title, form = comment_form, f=f )
 
 if __name__ == '__main__':
